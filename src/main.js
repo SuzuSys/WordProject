b7e78ab6bb102895ce1c98deb1b175128b583abc
SuzuSys <sigmatics@outlook.jp>
2022-02-26 20:54:33 +0900

    make change main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,31 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
+import Vue from 'vue';
+import App from './App.vue';
+import store from './store/user';
+import router from './router/index';
+import vuetify from './plugins/vuetify';
 
-Vue.config.productionTip = false
+import {
+  applyPolyfills,
+  defineCustomElements,
+} from '@aws-amplify/ui-components/loader';
+
+import Amplify, * as AmplifyModules from 'aws-amplify';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
+
+Vue.use(AmplifyModules);
+
+applyPolyfills().then(() => {
+  defineCustomElements(window);
+});
+
+Vue.config.ignoredElements = [/amplify-\w*/];
+
+Vue.config.productionTip = false;
 
 new Vue({
+  store,
+  router,
   vuetify,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
